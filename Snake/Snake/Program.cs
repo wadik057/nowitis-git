88915bf37fa3f0fbe6c3fadf6eee03a2a97a09bf
wadik057 <wadik057@gmail.com>
2016-05-13 15:22:48 +0300

    Упростили класс Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,23 +14,15 @@ namespace Snake
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
+            p1.Draw();
 
             Point p2 = new Point();
             p2.x = 4;
             p2.y = 5;
             p2.sym = '#';
-            Draw(p2.x, p2.y, p2.sym);
-
-           
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
