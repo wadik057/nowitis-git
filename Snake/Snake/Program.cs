dbc6527f8285be831286eba2d7f62920259d4900
wadik057 <wadik057@gmail.com>
2016-05-13 15:16:27 +0300

    Удалил старый код

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -22,15 +22,7 @@ namespace Snake
             p2.sym = '#';
             Draw(p2.x, p2.y, p2.sym);
 
-            /* int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);*/
-            
-            /*int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);*/
+           
 
             Console.ReadLine();
         }
