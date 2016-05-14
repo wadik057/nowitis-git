edbefbc42b6e8a2cd0534507bc6f3dc1abe7ae37
wadik057 <wadik057@gmail.com>
2016-05-14 20:04:14 +0300

    Осовили list

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -34,6 +34,10 @@ namespace Snake
 
             numList.RemoveAt(0);
 
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
     }
