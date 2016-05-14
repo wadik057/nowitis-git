d43ae8325bc0fa013d1aa500eb81df264d812e9f
wadik057 <wadik057@gmail.com>
2016-05-14 22:00:02 +0300

    Новая горизонтальная линия

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine();
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
             line.Drow();
 
             Console.ReadLine();
