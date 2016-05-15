139cddbe1ff9ccb5d89ced07051329aee511f833
wadik057 <wadik057@gmail.com>
2016-05-15 18:29:51 +0300

    В работе

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -13,7 +13,7 @@ namespace Snake
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
-            for(int x = xLeft; x <=xRight; x++, y++)
+            for(int x = xLeft; x <=xRight; x++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,6 +12,7 @@ namespace Snake
         {
             HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
             line.Drow();
+            VerticalLine Vline = new VerticalLine(11, 20, 6, '#');
 
             Console.ReadLine();
         }
